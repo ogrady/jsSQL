079de04d257d0adda62657cafd02555fb1d03553
Daniel O'Grady <daniel.ogrady@uni-tuebingen.de>
2019-01-21 11:39:17 +0100

    Make requirement of Util in object.js explicit

--- index.js
+++ index.js
@@ -2,36 +2,12 @@ const algebra = require("./evaluator/algebra.js");
 const generator = require("./evaluator/generator.js");
 const logic = require("./evaluator/logic.js");
 const object = require("./evaluator/object.js");
-
-const parser = require("./evaluator/parser.js");
-
-exports.printMsg = function() {
-  console.log("This is a message from the demo package");
-}
-
-/*
-console.log(algebra);
-console.log(generator);
-console.log(logic);
-console.log(object);
-console.log(parser);
-*/
-
-var l = new parser.RA.Lexer();
-var p = new parser.RA.Parser();
-var v = new parser.RA.Evaluator();
-let input = "A.x ≠ B";
-input = "A.x ⋉ B";
-input = "A ⋉{A.x = B.y} B";
-console.log(input);
-let lexd = l.lex(input);
-let parsed = p.parse(lexd);
-console.log(parsed);
-console.log(v.visit(parsed));
+const util = require("./evaluator/util.js");
 
 module.exports = {
     algebra: algebra,
     generator: generator,
     logic: logic,
-    object: object
+    object: object,
+    util: util
 };
